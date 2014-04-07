ebe934fef460c826f6ca38584349fa5af4f13ad0
BigRenegade <shantomken@yahoo.com>
2014-04-07 00:01:01 -0400

    added in new features, fixed bugs

--- LC_Control/stargate.lua
+++ LC_Control/stargate.lua
@@ -136,8 +136,8 @@ function compareVersions(v1,v2)
 		end
 
 		if not component.isAvailable("internet") then 
-			if not file_check(os.getenv("PWD") .. "gml.lua") or not file_check(os.getenv("PWD") .. "cv.gss") or not file_check(os.getenv("PWD") .. "default.gss") or not file_check(os.getenv("PWD") .. "gfxbuffer.lua") or not file_check(os.getenv("PWD") .. "colorutils.lua") or not file_check(os.getenv("PWD") .. "CompInfo.txt") then
-				io.stderr:write("You are missing one or more of the required files 'gml.lua', 'colorutils.lua', 'gfxbuffer.lua', 'CompInfo.txt', 'default.gss', or 'cv.gss' and do not have internet access to download them automaticly!\n")
+			if not file_check(os.getenv("PWD") .. "gml.lua") or not file_check(os.getenv("PWD") .. "cv.gss") or not file_check(os.getenv("PWD") .. "default.gss") or not file_check(os.getenv("PWD") .. "gfxbuffer.lua") or not file_check(os.getenv("PWD") .. "colorutils.lua") or not file_check(os.getenv("PWD") .. "GateInfo.txt") then
+				io.stderr:write("You are missing one or more of the required files 'gml.lua', 'colorutils.lua', 'gfxbuffer.lua', 'default.gss', or 'cv.gss' and do not have internet access to download them automaticly!\n")
 				return
 			end
 		else
@@ -157,15 +157,6 @@ if (compareVersions(remoteVersion(), localVersion()) == 0) then
 end
 --We've checked for gml, and downloaded it if it was available, so we can load gml now.
 
-
-
-
-
-
-
-
-
-
 local gml=require("gml")
 
 local gpu=component.gpu
@@ -174,6 +165,7 @@ local filename = shell.resolve(fname)
 local connectAddress = ""
 local destAddress = {}
 local destName = {}
+local timerID = 0
 
 local function isAdvanced()
 	return gpu.getDepth()
@@ -184,7 +176,6 @@ local function strripos(s, delim)
 end
 
 function loadAddressData()
-  local indx = 1
 	do
 		local f = io.open(filename)
 		if f then
@@ -199,38 +190,117 @@ function loadAddressData()
 	end
 end
 
+
+
+
 local sg = component.stargate
 
 local gui=gml.create("center", 1, 160, 50)
 gui.style=gml.loadStyle("cv.gss")
 
-local dialing = ""
-
-if (sg.isDialing()) then
-	dialing = "True"
-else
-	dialing = "False"
+local function dialing()
+  return tostring(sg.isDialing())
 end
 
-if (sg.isValidAddress("DACEABGAA")) then
-	valid = "True"
-else
-	valid = "False"
+local function connected()
+  return tostring(sg.isConnected())
 end
 
-if (sg.isConnected()) then
-	connected = "True"
-else
-	connected = "False"
+local function valid(address)
+  local reply, error = sg.isValidAddress(address)
+  if reply == nil then
+    return (error)
+  else
+    return (tostring(reply))
+  end
 end
+ 
 
 
 loadAddressData()
 
-local addressList=gui:addListBox(17,-22,32,11, destName)
-local addressLabel=gui:addLabel(52, -32, 25,"Address:  "..destAddress[1])
-local dialingLabel=gui:addLabel(2, 3, 25,"Dialing:  "..dialing)
-local validLabel=gui:addLabel(2, 5, 25, "Valid Address:  "..valid)
+local addressListBoxGUI = gml.create(65, 16, 70, 20)
+local addressList=gui:addListBox(75,-22,50,11, destName)
+local addressLabel=gui:addLabel(75, -19, 35,"Destination Address:  "..destAddress[1])
+local validLabel=gui:addLabel(75, -17, 50, "Valid Address:  "..valid(destAddress[1]))
+
+local dialingLabel=gui:addLabel(40, 3, 13,"Dialing:  ")
+local dialingActiveLabel=gui:addLabel(54, 3, 7,"       ")
+
+local contentsLabel=gui:addLabel(10, 3, 30, "Gate Address:  "..sg.getAddress())
+local connectedLabel=gui:addLabel(70, 3, 18, "Gate Connected:  ")
+local connectedActiveLabel=gui:addLabel(89, 3, 7, "       ")
+
+local newAddressGUI = gml.create(8, 16, 50, 20)
+local newAddressLabel=gui:addLabel(15, -31, 13,"Address Name:")
+local newAddressField=gui:addTextField(15,-28,30)
+local newGateAddressLabel=gui:addLabel(15, -26, 13,"Gate Address:")
+local newGateAddressField=gui:addTextField(15,-22,9)
+
+function saveAddressData()
+  destName[#destName + 1] = text.trim(newAddressField.text)
+  destAddress[#destAddress + 1] = text.trim(newGateAddressField.text)
+	do
+		local f = io.open(filename, "w")
+		if f then
+			for a = 1, #destName do
+        f:write(tostring(destName[a])..","..tostring(destAddress[a]).."\n")
+			end
+			f:close()
+		end
+	end
+  addressList:updateList(destName)
+end
+
+local newAddressSave=gui:addButton(21,-18,10,1,"Save", saveAddressData)
+local newAddressCancel=gui:addButton(37,-18,10,1,"Cancel",gui.close)
+
+
+gui.onRun=function()
+  newAddressGUI:draw()
+  newAddressLabel:draw()
+  newAddressField:draw()
+  newGateAddressLabel:draw()
+  newGateAddressField:draw()
+  newAddressSave:draw()
+  newAddressCancel:draw()
+  addressListBoxGUI:draw()
+  addressList:draw()
+  addressLabel:draw()
+  validLabel:draw()
+  dialingLabel:draw()
+  dialingActiveLabel:draw()
+
+end
+
+
+
+local function updateValues()
+  event.cancel(timerID)
+  connectedActiveLabel.text=string.sub(" "..connected().."  ", 1, 7)
+  dialingActiveLabel.text= string.sub(" "..dialing().."  ", 1, 7)
+
+  if (text.trim(connected()) == "false") then
+    connectedActiveLabel["text-color"] = 0xFFFFFF
+    connectedActiveLabel["text-background"] = 0xBB0000
+  else
+    connectedActiveLabel["text-color"] = 0xFFFFFF
+    connectedActiveLabel["text-background"] = 0x009900
+  end
+  if (text.trim(dialing()) == "false") then
+    dialingActiveLabel["text-color"] = 0xFFFFFF
+    dialingActiveLabel["text-background"] = 0xBB0000
+  else
+    dialingActiveLabel["text-color"] = 0xFFFFFF
+    dialingActiveLabel["text-background"] = 0x009900
+  end
+
+  connectedLabel:draw()
+  connectedActiveLabel:draw()
+  dialingLabel:draw()
+  dialingActiveLabel:draw()
+end
+
 
 local function updateDialAddress(info)
   local addressID = 0
@@ -245,34 +315,35 @@ local function updateDialAddress(info)
   end
   if addressID ~= 0 then
     connectAddress = text.trim(destAddress[addressID])
-    addressLabel.text="Address "..destAddress[addressID]
+    addressLabel.text="Destination Address: "..destAddress[addressID]
     addressLabel:draw()
   end
-  if (sg.isValidAddress(connectAddress)) then
-    valid = "True"
-  else
-    valid = "False"
-  end
-  validLabel.text= "Valid Address:  "..valid
+  validLabel.text= "Valid Address:  "..valid(connectAddress)
   validLabel:draw()
+end
+
+
+
+
+local function disconnect()
+  sg.disconnect()
+  os.sleep(2)
+  updateValues()
 end
 
 local function dialGate()
   sg.dial(connectAddress)
-  if (sg.isDialing()) then
-    dialing = "True"
-  else
-    dialing = "False"
-  end
-  dialingLabel.text= "Dialing:  "..dialing
-  dialingLabel:draw()
+  dialingActiveLabel["text-color"] = 0xFFFFFF
+  dialingActiveLabel["text-background"] = 0x009900
+  dialingActiveLabel.text= string.sub(" "..dialing().."  ", 1, 7)
+  dialingActiveLabel:draw()
+  timerID = event.timer(26, updateValues, 1)
 end
 
-local contentsLabel=gui:addLabel(40, 3, 25, "Address:  "..sg.getAddress())
-local connectedLabel=gui:addLabel(40, 5, 25, "Connected:  "..connected)
+
 
 local connect=gui:addButton(5,-3,10,1,"Connect",dialGate)
-local disconnect=gui:addButton(25,-3,10,1,"Disconnect",sg.disconnect)
+local disconnect=gui:addButton(25,-3,10,1,"Disconnect", disconnect)
 local refuel=gui:addButton(45,-3,10,1,"Refuel",gui.close)
 local close=gui:addButton(65,-3,10,1,"Close",gui.close)
 
@@ -287,7 +358,7 @@ addressList.onDoubleClick=function()
   updateDialAddress(addressList:getSelected())
 end
 
-
+updateValues()
 gui:run()
 
 gpu.setForeground(0xFFFFFF)
