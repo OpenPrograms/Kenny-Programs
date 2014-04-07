c0660d4972c26eda588fdeb2310e5ef3170271df
BigRenegade <shantomken@yahoo.com>
2014-04-07 13:22:54 -0400

    finished adding in remaining functions, updated version

--- LC_Control/stargate.lua
+++ LC_Control/stargate.lua
@@ -11,7 +11,6 @@ local unicode = require("unicode")
 local sides = require("sides")
 local colors=require("colors")
 
-
 -- The following code added by Michiyo, to check if:
 -- A.) The computer has HTTP access, and 
 -- B.) if the required files exist to run this program.
@@ -159,13 +158,19 @@ end
 
 local gml=require("gml")
 
+local sg = component.stargate
 local gpu=component.gpu
+local w, h = gpu.getResolution()
 local fname = "GateInfo.txt"
 local filename = shell.resolve(fname)
 local connectAddress = ""
-local destAddress = {" "}
-local destName = {" "}
+local destAddress = {}
+local destName = {}
 local timerID = 0
+local fuelTimer = 0
+local running=shell.running() 
+local addressListAdd
+local addressListDelete
 
 local function isAdvanced()
 	return gpu.getDepth()
@@ -176,6 +181,8 @@ local function strripos(s, delim)
 end
 
 function loadAddressData()
+  destName = {}
+  destAddress = {}
 	do
 		local f = io.open(filename)
 		if f then
@@ -188,14 +195,14 @@ function loadAddressData()
 			f:close()
 		end
 	end
+  if (#destName < 1) then
+    destName = {" "}
+    destAddress = {" "}
+  end
 end
 
-
-
-
-local sg = component.stargate
-
-local gui=gml.create("center", 1, 160, 50)
+term.clear()
+local gui=gml.create("center", "center", 80, 25)
 gui.style=gml.loadStyle("cv.gss")
 
 local function dialing()
@@ -204,6 +211,10 @@ end
 
 local function connected()
   return tostring(sg.isConnected())
+end
+
+local function fuelAvailable()
+  return tostring(sg.hasFuel())
 end
 
 local function valid(address)
@@ -215,31 +226,49 @@ local function valid(address)
   end
 end
  
-
-
 loadAddressData()
 
-local addressListBoxGUI = gml.create(65, 16, 70, 20)
-local addressList=gui:addListBox(75,-22,50,11, destName)
-local addressLabel=gui:addLabel(75, -19, 35,"Destination Address:  "..destAddress[1])
-local validLabel=gui:addLabel(75, -17, 50, "Valid Address:  "..valid(destAddress[1]))
+local addressListBoxGUI = gml.create("center", 18, 48, 16)
+local addressList=gui:addListBox(21, 7, 38, 8, destName)
+local addressLabel=gui:addLabel(21, 16, 35,"Destination:  "..text.trim(destAddress[1]))
+local validLabel=gui:addLabel(21, 18, 35, "Valid:  "..valid(text.trim(destAddress[1])))     
 
-local dialingLabel=gui:addLabel(40, 3, 13,"Dialing:  ")
-local dialingActiveLabel=gui:addLabel(54, 3, 7,"       ")
+local contentsLabel=gui:addLabel(5, 2, 15, "Gate Address:")
+local contentsActiveLabel=gui:addLabel(20, 2, 9, "         ")
+contentsActiveLabel.text = text.trim(sg.getAddress())
+contentsActiveLabel["text-color"] = 0xFFFFFF
+contentsActiveLabel["text-background"] = 0x0000AA
+contentsActiveLabel:draw()
 
-local contentsLabel=gui:addLabel(10, 3, 30, "Gate Address:  "..sg.getAddress())
-local connectedLabel=gui:addLabel(70, 3, 18, "Gate Connected:  ")
-local connectedActiveLabel=gui:addLabel(89, 3, 7, "       ")
+local fuelLabel=gui:addLabel(35, 2, 30, "Has Fuel:")
+local fuelLabelActive=gui:addLabel(48, 2, 7, "looking")
 
-local newAddressGUI = gml.create(8, 16, 50, 20)
-local newAddressLabel=gui:addLabel(15, -31, 13,"Address Name:")
-local newAddressField=gui:addTextField(15,-28,30)
-local newGateAddressLabel=gui:addLabel(15, -26, 13,"Gate Address:")
-local newGateAddressField=gui:addTextField(15,-22,10)
+local dialingLabel=gui:addLabel(5, 4, 13,"Dialing:  ")
+local dialingActiveLabel=gui:addLabel(20, 4, 7,"       ")
+local connectedLabel=gui:addLabel(35, 4, 17, "Connected:  ")
+local connectedActiveLabel=gui:addLabel(48, 4, 7, "       ")
+
+local function hasFuel()
+  if (text.trim(fuelAvailable()) == "false") then
+    fuelLabelActive["text-color"] = 0xFFFFFF
+    fuelLabelActive["text-background"] = 0xBB0000
+  else
+    fuelLabelActive["text-color"] = 0xFFFFFF
+    fuelLabelActive["text-background"] = 0x009900
+  end
+  fuelLabelActive.text = fuelAvailable()
+  fuelLabelActive:draw()
+end
 
 function saveAddressData()
-  destName[#destName + 1] = text.trim(newAddressField.text)
-  destAddress[#destAddress + 1] = text.trim(newGateAddressField.text)
+  if (string.len(text.trim(newAddressField.text)) > 1) then
+    destName[#destName + 1] = text.trim(newAddressField.text)
+    destAddress[#destAddress + 1] = text.trim(newGateAddressField.text)
+    newAddressField.text = ""
+    newGateAddressField.text = ""
+    newAddressField:draw()
+    newGateAddressField:draw()
+  end
 	do
 		local f = io.open(filename, "w")
 		if f then
@@ -252,28 +281,73 @@ function saveAddressData()
   addressList:updateList(destName)
 end
 
-local newAddressSave=gui:addButton(21,-18,10,1,"Save", saveAddressData)
-local newAddressCancel=gui:addButton(37,-18,10,1,"Cancel",gui.close)
-
-
-gui.onRun=function()
-  newAddressGUI:draw()
-  newAddressLabel:draw()
+local function cancelAddress()
+  newAddressField.text = ""
+  newGateAddressField.text = ""
   newAddressField:draw()
-  newGateAddressLabel:draw()
   newGateAddressField:draw()
-  newAddressSave:draw()
-  newAddressCancel:draw()
+end
+
+local function deleteAddress()
+  for id, name in pairs(destName) do
+    if text.trim(name) == text.trim(addressList:getSelected()) then
+      addressID = id
+      break
+    end
+  end
+  if addressID ~= 0 then
+    table.remove(destName, addressID)
+    table.remove(destAddress, addressID)
+    destName[addressID] = nil
+    destAddress[addressID] = nil
+  end
+  addressList:updateList(destName)
+  saveAddressData()
+end
+
+local function addAddress()
+  gpu.setForeground(0xFFFFFF)
+  gpu.setBackground(0x000000)
+  term.clear()
+  newAddressGUI = gml.create("center", "center", 50, 16)
+  newAddressLabel=newAddressGUI:addLabel(2, 2, 13,"Address Name:")
+  newAddressField=newAddressGUI:addTextField(15, 2, 30)
+  newGateAddressLabel=newAddressGUI:addLabel(2, 4, 13,"Gate Address:")
+  newGateAddressField=newAddressGUI:addTextField(15, 4, 10)
+  newAddressSave=newAddressGUI:addButton(5, 6, 10, 1, "Save", saveAddressData)
+  newAddressCancel=newAddressGUI:addButton(20, 6, 10, 1, "Cancel", cancelAddress)
+  newAddressClose=newAddressGUI:addButton(35, 6, 10, 1, "Close", newAddressGUI.close)
+  newAddressGUI:run()
+  term.clear()
+  gui:draw()
   addressListBoxGUI:draw()
   addressList:draw()
   addressLabel:draw()
+  addressListAdd:draw()
+  addressListDelete:draw()
   validLabel:draw()
   dialingLabel:draw()
   dialingActiveLabel:draw()
-
 end
 
+addressListAdd=gui:addButton(25, 20, 10, 1, "Add", addAddress)
+addressListDelete=gui:addButton(45, 20, 10, 1, "Delete", deleteAddress)
 
+gui.onClose=function()
+  event.cancel(fuelTimer)
+end
+
+gui.onRun=function()
+  fuelID = event.timer(20, hasFuel, math.huge)
+  addressListBoxGUI:draw()
+  addressList:draw()
+  addressLabel:draw()
+  addressListAdd:draw()
+  addressListDelete:draw()
+  validLabel:draw()
+  dialingLabel:draw()
+  dialingActiveLabel:draw()
+end
 
 local function updateValues()
   event.cancel(timerID)
@@ -301,7 +375,6 @@ local function updateValues()
   dialingActiveLabel:draw()
 end
 
-
 local function updateDialAddress(info)
   local addressID = 0
   local addInfo = text.trim(info)
@@ -322,9 +395,6 @@ local function updateDialAddress(info)
   validLabel:draw()
 end
 
-
-
-
 local function disconnect()
   sg.disconnect()
   os.sleep(2)
@@ -340,13 +410,14 @@ local function dialGate()
   timerID = event.timer(26, updateValues, 1)
 end
 
+local function onAddressDial()
+  updateDialAddress(addressList:getSelected())
+  dialGate()
+end
 
-
-local connect=gui:addButton(5,-3,10,1,"Connect",dialGate)
-local disconnect=gui:addButton(25,-3,10,1,"Disconnect", disconnect)
-local refuel=gui:addButton(45,-3,10,1,"Refuel",gui.close)
-local close=gui:addButton(65,-3,10,1,"Close",gui.close)
-
+local connect=gui:addButton(15,-1,10,1,"Connect",onAddressDial)
+local disconnect=gui:addButton(35,-1,10,1,"Disconnect", disconnect)
+local close=gui:addButton(55,-1,10,1,"Close",gui.close)
 
 local function onAddressSelect(lb,prevIndex,selIndex)
   updateDialAddress(addressList:getSelected())
